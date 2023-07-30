906ea41f7c8960bc4a8efe813a02331d334ac546
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-07-30 18:53:14 -0400

    updated testimonial

--- src/Components/Pages/Testimonial/Testimonial.jsx
+++ src/Components/Pages/Testimonial/Testimonial.jsx
@@ -12,17 +12,18 @@ const Testimonial = () =>{
     return(
         <div className="testimonial">
             <div className="testimonial-container">
-                <h1>My Testimonial</h1>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum odit reprehenderit velit optio maxime autem, mollitia modi exercitationem distinctio at ullam doloribus iste quos laborum, similique enim incidunt alias rerum.</p>
+                <div className="test-heading">
+                    <h1>My Testimonial</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum odit reprehenderit velit optio maxime autem, mollitia modi exercitationem distinctio at ullam doloribus iste quos laborum, similique enim incidunt alias rerum.</p>
+                </div>
                 <div>
-                     <Swiper
-                        modules={[Navigation, Pagination, Scrollbar, Autoplay]}
-                        spaceBetween={40}
+                    <Swiper className="swiper"
+                        modules={[  Autoplay]}
+                        spaceBetween={20}
                         slidesPerView={2}
                         // navigation
                         autoplay={{Autoplay: true}}
                         pagination={{ clickable: true }}
-                        // scrollbar={{ draggable: true }}
                         onSwiper={(swiper) => console.log(swiper)}
                         onSlideChange={() => console.log('slide change')}
                         breakpoints={{
@@ -52,7 +53,7 @@ const Testimonial = () =>{
                             <div className="slider-top">
                                 <img src={adam} alt="" />
                                 <div>
-                                    <h2>Assayouti Adams</h2>
+                                    <h2>Idris Abdulkareem</h2>
                                     <p>Regular Client</p>
                                 </div>
                             </div>
@@ -62,17 +63,13 @@ const Testimonial = () =>{
                             <div className="slider-top">
                                 <img src={adam} alt="" />
                                 <div>
-                                    <h2>Assayouti Adams</h2>
+                                    <h2>Albert Rice</h2>
                                     <p>Regular Client</p>
                                 </div>
                             </div>
                             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, earum. Obcaecati nam numquam modi repellendus necessitatibus nulla eaque quas temporibus est fugit. Corporis tempore architecto, voluptatibus esse recusandae quibusdam sint.</p>
                         </SwiperSlide>
-                        {/* <SwiperSlide>Slide 2</SwiperSlide>
-                        <SwiperSlide>Slide 3</SwiperSlide>
-                        <SwiperSlide>Slide 4</SwiperSlide> */}
-                        ...
-                        </Swiper>
+                    </Swiper>
                 </div>
             </div>
         </div>
