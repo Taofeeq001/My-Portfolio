3fdaacf484e0238ca6762c8bfacba5f7ff6b8b31
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-07-30 19:20:07 -0400

    updating hero AOS

--- src/Components/Pages/Hero/Hero.jsx
+++ src/Components/Pages/Hero/Hero.jsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, {useEffect} from "react";
 import './Hero.css'
 import Stars from "../../../Images/star.png";
 import picture from '../../../Images/person.png'
+import 'aos/dist/aos.css';
+import AOS from 'aos';
 
 
 const Hero = ()=>{
+    useEffect(()=>{
+        AOS.init({
+            offset: 200,
+            duration: 800,
+            easing:'ease-in-out',
+            delay: 0,
+        });
+    },[])
     return(
         <div className="hero">
             <div className="hero-content">
                 <div className="content-paragraph">
                     <div className="content-detail">
-                        <div>
+                        <div data-aos="fade-right">
                             <h4>Hello, My name is Taofeeq Olayiwola Kolapo</h4>
                             <h2>I'm a Professional Web Developer</h2>
                         </div>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam reprehenderit perferendis dolore. Voluptas a quaerat animi veritatis illo voluptatibus similique eligendi dolorem veniam magnam. Est facilis nam aut molestiae earum!</p>
+                        <p data-aos = "fade-in">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam reprehenderit perferendis dolore. Voluptas a quaerat animi veritatis illo voluptatibus similique eligendi dolorem veniam magnam. Est facilis nam aut molestiae earum!</p>
                     </div>
-                    <div>
+                    <div data-aos = "slide-up">
                         <button><a href="">DOWNLOAD CV</a></button>    
                     </div>
                 </div>
-                <img src={picture} alt="" />
+                <img data-aos = "zoom-in" src={picture} alt="" />
             </div>
             <div className="stars">
                 <img src={Stars} alt="" />
--- src/Components/Pages/Nav/Nav.jsx
+++ src/Components/Pages/Nav/Nav.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useEffect} from "react";
 import './Nav.css'
 import {AiOutlineUnorderedList} from 'react-icons/ai'
+
 
 const Nav =()=>{
     return(
@@ -10,6 +11,13 @@ const Nav =()=>{
                     <h3>SimSuraj Dev</h3>
                 </div>
                 <div className="desktop-nav">
+                    <a href="">Home</a>
+                    <a href="">About</a>
+                    <a href="">Portfolio</a>
+                    <a href="">Contact Me</a>
+                    <AiOutlineUnorderedList/>
+                </div>
+                <div className="mobile-nav">
                     <a href="">Home</a>
                     <a href="">About</a>
                     <a href="">Portfolio</a>
