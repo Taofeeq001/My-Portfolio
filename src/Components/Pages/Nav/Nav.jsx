445a2cb337c60dd5d905b82da71b11454e9cec39
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-07-31 08:46:50 -0400

    updated responsiveness

--- src/Components/Pages/Nav/Nav.jsx
+++ src/Components/Pages/Nav/Nav.jsx
@@ -1,30 +1,43 @@
-import React, {useEffect} from "react";
+import React, {useState} from "react";
 import './Nav.css'
 import {AiOutlineUnorderedList} from 'react-icons/ai'
 
 
 const Nav =()=>{
+    // const [deskToggle, setDeskToggle] = useState(false)
+    const [mobiltog, setMobilTog] = useState(false)
+    
+    // const handleClick = () =>{
+    //     setDeskToggle(!deskToggle)
+    // }
+    const handleClick = () =>{
+        setMobilTog(!mobiltog)
+    }
     return(
-        <div>
+        <div className="navigator">
             <nav className="navigation">
                 <div>
                     <h3>SimSuraj Dev</h3>
                 </div>
                 <div className="desktop-nav">
-                    <a href="">Home</a>
-                    <a href="">About</a>
-                    <a href="">Portfolio</a>
-                    <a href="">Contact Me</a>
-                    <AiOutlineUnorderedList/>
-                </div>
-                <div className="mobile-nav">
-                    <a href="">Home</a>
-                    <a href="">About</a>
-                    <a href="">Portfolio</a>
-                    <a href="">Contact Me</a>
-                    <AiOutlineUnorderedList/>
+                    <div className="desktop-navs">
+                        <a href="">Home</a>
+                        <a href="">About</a>
+                        <a href="">Portfolio</a>
+                        <a href="">Contact Me</a>
+                    </div>
+                    <button className="toggle" onClick={handleClick}><AiOutlineUnorderedList/></button>
                 </div>
             </nav>
+            <div className={mobiltog? "mobile-sub":'mobile-nav'}>
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Portfolio</a>
+                <a href="">Contact Me</a>
+            </div>
+            <div className="right-side">
+                
+            </div>
         </div>
     )
 }
